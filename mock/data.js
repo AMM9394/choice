8aab8f67910374aebadcc29cff0a3ee80f1ce306
GuoMiaomiao <guomiaomiao@fiberhome.com>
2018-04-26 18:53:28 +0800

    xiugai

---
+++ mock/data.js
@@ -0,0 +1,8 @@
+const rootTypes = ['SHARP','InFocus'];
+const sub1Types = ['938','918','828','638','V500','V600'];
+const sub2Types = ['LCD-60SU470A-01','LCD-60SU470A-01','LCD-70DS8008A-01','LCD-60SU470A-01','LCD-60SU470A-01','LCD-60SU470A-01'];
+const data = [
+  rootTypes.map((v,k)=>{
+
+  })
+]
